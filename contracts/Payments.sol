4d7d4a6de917ebd0642f8c579ed194d7ee023e4c
ART1KZ <artemkiselev18072k6@mail.ru>
2025-01-31 18:41:53 +0500

    add payments contract

--- contracts/Payments.sol
+++ contracts/Payments.sol
@@ -3,5 +3,45 @@
 pragma solidity ^0.8.28;
 
 contract Payments {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    struct Payment {
+        uint amount;
+        uint timestamp;
+        address from;
+        string message;
+    }
+
+    struct Balance {
+        uint totalPayments;
+        mapping(uint => Payment) payments;
+    }
+
+    mapping(address => Balance) balances;
+
+    function getMyBalance() public view returns (uint256) {
+        return msg.sender.balance;
+    }
+
+    function pay(string memory message) public payable {
+        uint paymentNum = balances[msg.sender].totalPayments;
+        balances[msg.sender].totalPayments++;
+
+        Payment memory newPayment = Payment(
+            msg.value,
+            block.timestamp,
+            msg.sender,
+            message
+        );
+        
+        balances[msg.sender].payments[paymentNum] = newPayment;
+    }
     
+    function getPayment(address _addr, uint _index) public view returns (Payment memory) {
+        return balances[_addr].payments[_index];
+    }
 }
